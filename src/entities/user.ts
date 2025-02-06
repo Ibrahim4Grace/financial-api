4fc1106d5bef87b116be200816e7bb540c021a4f
Agboola Akorede <akoredeagboola23@gmail.com>
2025-02-06 18:00:22 +0100

    feat:include enitity

---
+++ src/entities/user.ts
@@ -0,0 +1,69 @@
+import * as bcrypt from 'bcrypt';
+import {
+  Entity,
+  BeforeInsert,
+  BeforeUpdate,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Column,
+  BaseEntity,
+} from 'typeorm';
+
+@Entity('users')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password!: string;
+
+  @Column({ default: false })
+  isEmailVerified: boolean;
+
+  @Column({ type: 'jsonb', nullable: true })
+  emailVerificationOTP: {
+    otp: string;
+    verificationToken: string;
+    expiresAt: Date;
+  } | null;
+
+  @Column({ type: 'jsonb', default: [] })
+  passwordHistory: Array<{
+    password: string;
+    changedAt: Date;
+  }>;
+
+  @Column({ default: 0 })
+  failedLoginAttempts: number;
+
+  @Column({ default: false })
+  isLocked: boolean;
+
+  @Column('text', { default: 'admin' })
+  role: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (this.password && this.password.length > 0) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+  }
+
+  async comparePassword(candidatePassword: string): Promise<boolean> {
+    return bcrypt.compare(candidatePassword, this.password);
+  }
+}
